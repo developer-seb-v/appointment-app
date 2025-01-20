c00860be9a1735ea9c3a9c1cf8a0abc6e798b99f
Sebastian Valenzuela <developer.seb.v@gmail.com>
2025-01-19 18:44:26 -0800

    last commit. app completed. Added boostrap stylings and local storage for demo purposes

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,17 +1,51 @@
 import { Component } from '@angular/core';
 import { Appointment } from '../models/appointment';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
   styleUrls: ['./appointment-list.component.css']
 })
-export class AppointmentListComponent {
+export class AppointmentListComponent implements OnInit {
+  
 
-appointment: Appointment  = {
-  id: 1,
-  title: "Take dog for a walk",
-  date: new Date('2025-1-19')
+  newAppointmentTitle = ''
+  newAppointmentDate: Date = new Date()
+  appointments: Appointment[] = []
+
+  ngOnInit(): void {
+   let savedAppointments = localStorage.getItem("appointments")
+
+   // newer syntax to check if there are any saved appointments 
+   // and if true parse the JSON
+   // if false create new empty array []
+    this.appointments = savedAppointments? JSON.parse(savedAppointments) : []
+  }
+
+  addAppointment() {
+    if (this.newAppointmentTitle.trim().length && this.newAppointmentDate) {
+      let newAppointment: Appointment = {
+        id: Date.now(),
+        title: this.newAppointmentTitle,
+        date: this.newAppointmentDate
+      }
+      this.appointments.push(newAppointment)
+
+      this.newAppointmentTitle = "";
+      this.newAppointmentDate = new Date()
+
+      localStorage.setItem("appointments", JSON.stringify(this.appointments))
+    }
+
+
+
+  }
+  deleteAppointment(index: number) {
+
+    this.appointments.splice(index, 1)
+    localStorage.setItem("appointments", JSON.stringify(this.appointments))
+
+  }
 }
 
-}
